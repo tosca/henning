4af484d11803a112c23b2793c4c8ad8b04d7b3bd
tosca ragnini <tosca.ragnini@gmail.com>
2015-07-12 22:20:51 -0500

    publish first synch

---
+++ henning/henning/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(henning.Startup))]
+namespace henning
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
