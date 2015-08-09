bb4d97f448cf8686acba214dcf72f2498cf7cd56
tosca ragnini <tosca.ragnini@gmail.com>
2015-08-08 22:57:15 -0500

    removed first name from form

--- henning/henning/Controllers/HomeController.cs
+++ henning/henning/Controllers/HomeController.cs
@@ -44,13 +44,12 @@ namespace henning.Controllers
         public ActionResult Contact(ContactInfo contact)
         {
             ViewBag.Message = "Your contact page.";
-
-            var firstName = contact.FirstName;
+           
             var fullName = contact.FullName;
             var contactEmail = contact.ContactEmail;
             var contactPhone = contact.ContactPhone;
             var contactMessage = contact.ContactMessage;
-          SendEmail(firstName, fullName, contactEmail, contactPhone, contactMessage);
+          SendEmail(fullName, contactEmail, contactPhone, contactMessage);
             return RedirectToAction("Index");
         }
 
@@ -63,7 +62,7 @@ namespace henning.Controllers
         }
 
 
-        public void SendEmail(string firstName, string fullName, string contactEmail, string contactPhone, string contactMessage)
+        public void SendEmail(string fullName, string contactEmail, string contactPhone, string contactMessage)
         {
 
             RestClient client = new RestClient();
@@ -80,7 +79,6 @@ namespace henning.Controllers
             //request.AddParameter("to", "bar@example.com");
             request.AddParameter("to", "tosca.ragnini@gmail.com");
             request.AddParameter("subject", "Grand Legacy - Contact Request");
-            request.AddParameter("text", "FirstName : " + firstName);
             request.AddParameter("text", "FullName : " + fullName);
             request.AddParameter("text", "ContactEmail : " + contactEmail);
             request.AddParameter("text", "ContactPhone : " + contactPhone);
@@ -96,7 +94,6 @@ namespace henning.Controllers
 
     public class ContactInfo
     {
-        public string FirstName { get; set; }
         public string FullName { get; set; }
         public string ContactEmail { get; set; }
         public string ContactPhone { get; set; }
