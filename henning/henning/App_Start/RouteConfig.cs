6e916056838503bf9540a2c2a8e09bb7d3f02d11
tosca ragnini <tosca.ragnini@gmail.com>
2017-05-22 10:11:52 -0500

    remove leeward llc

--- henning/henning/App_Start/RouteConfig.cs
+++ henning/henning/App_Start/RouteConfig.cs
@@ -18,16 +18,12 @@ namespace henning
                 url: "grand-legacy",
                 defaults: new { controller = "Home", action = "grand", id = UrlParameter.Optional }
             );
-            routes.MapRoute(
-                name: "stone",
-                url: "stone",
-                defaults: new { controller = "Home", action = "stone", id = UrlParameter.Optional }
-            );
+        
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+               defaults: new { controller = "Home", action = "grand", id = UrlParameter.Optional }
+           );
         }
     }
 }
